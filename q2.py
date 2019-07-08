5d6f9d3da29bb396b642b96f441639e631cdf74c
reddzzz <43365969+reddzzz@users.noreply.github.com>
2019-07-08 19:44:55 +0700

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,41 +1,32 @@
 import math
 import numpy as np
 
-print("\n\n#2")         #root equation algorithm
+print("#2")         #root equation algorithm
 
-def f_1(x):
-    return math.pow(x, 3) + 7 * math.pow(x, 2) - 36
-#bisection method
-def bisection(f, a, b, e0):
-    if (f(a) * f(b) >= 0):
-        print("You have not assumed right a and b\n")
-        return
+def f(x):
+    return math.pow(x,3) + 7 *math.pow(x,2) - 36
+def df(x):
+    return 3*math.pow(x,2) + 14*x
+def bi(f,a,b,e0):
+    if(f(a) * f(b) < 0):
+        while True:
+            c = 0.5 * (a + b)
+            if(f(a) * f(c) < 0):
+                b = c
+            else:
+                a = c
+            if np.abs(f(c) < e0):
+                break
+        return c
 
-    c = a
-    while ((b - a) >= e0):
-        c = (a + b) / 2
-        if (f(c) == 0.0):
+print(bi(f,-4,-2,0.05))
+
+def new(f,f1,x,e0):
+    while True:
+        x = x - f(x)/f1(x)
+        if np.abs(f(x)) < e0:
             break
-        if (f(c) * f(a) < 0):
-            b = c
-        else:
-            a = c
-
-    return c
-
-print("Bisection algorithm: ", bisection(f_1, -4, -2, 0.05))
-
-def f1_1(x):
-    return 3 * math.pow(x, 2) + 14 * x
-
-#newton raphson method
-def newton_raphson(f, f1, x, es):
-    h = f(x) / f1(x)
-
-    while abs(h) >= es:
-        h = f(x) / f1(x)
-        x = x - h
-
     return x
 
-print("Newton Rhapson algorithm: ", newton_raphson(f_1, f1_1, -3, 0.05))
+
+print(new(f,df,-3,0.05))
